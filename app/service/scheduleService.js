ff92bf8269560f08bffaedc3f7c4ed490be22433
jerry_sp <731238433@qq.com>
2020-12-15 16:56:32 +0800

    [修改bug]同处理方法多个任务下的数据查询不准确

--- app/service/scheduleService.js
+++ app/service/scheduleService.js
@@ -6,8 +6,16 @@ class ScheduleService extends Service {
   /**
    * 测试
    */
-  async testHandler(id) {
-    await this.logger.info('我是测试任务，任务ID：%s', id);
+  async testHandler(job_id) {
+    // 读取锁
+    const locked = await this.app.redlock.lock('sendAllUserBroadcast:' + id, 'sendAllUserBroadcast', 180);
+    if (!locked) return false;
+
+    const schedule = await this.app.mysql.get('schedule_job', { job_id });
+    await this.logger.info('我是测试任务，任务信息: %j', schedule);
+
+    // 释放锁
+    await this.app.redlock.unlock('sendAllUserBroadcast:' + id);
   }
 }
 
