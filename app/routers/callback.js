33f8024968f60cd4857039c9dea090acebaa3e20
Jerry <731238433@qq.com>
2023-09-02 16:36:50 +0800

    测试华为推送回调

--- app/routers/callback.js
+++ app/routers/callback.js
@@ -7,5 +7,5 @@ module.exports = app => {
   const { router, controller, config } = app;
 
   // 华为回调
-  router.all(`${config.contextPath}/huawei`, controller.callback.huawei);
+  router.all(`${config.contextPath}/callback/huawei`, controller.callback.huawei);
 };
